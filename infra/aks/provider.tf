ef9616e8d3d4dcc2bf8d4311dab139a01ed4ed40
Ramachandran K <Ramachandran.K@tui.co.uk>
2024-04-07 12:52:50 +0530

    provider updated

--- infra/aks/provider.tf
+++ infra/aks/provider.tf
@@ -11,6 +11,9 @@ terraform {
     storage_account_name  = "tfstatecprime"
     container_name        = "cprimestate"
     key                   = "cprime.terraform.tfstate"
+    client_id       = var.arm_client_id
+    client_secret   = var.arm_client_secret
+    tenant_id       = var.arm_tenant_id
   }
 }
 
